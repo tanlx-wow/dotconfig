c1488af3180549c6a6872b1c55c04d1ffda0641c
tanlx-wow <tanlx@mail.uc.edu>
2025-07-13 16:09:54 -0400

    update

---
+++ wezterm/constants.lua
@@ -0,0 +1,7 @@
+local M = {}
+
+M.GL_even = os.getenv("HOME") .. "/.config/wezterm/assets/GL_even_8bit.png"
+
+M.bg_img = M.GL_even
+
+return M
---
+++ wezterm/wezterm.lua
@@ -0,0 +1,51 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+local constants = require("constants")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+local home = wezterm.home_dir
+
+-- coolnight colorscheme
+
+config.colors = {
+	foreground = "#CBE0F0",
+	background = "#011423",
+	cursor_bg = "#47FF9C",
+	cursor_border = "#47FF9C",
+	cursor_fg = "#011423",
+	selection_bg = "#033259",
+	selection_fg = "#CBE0F0",
+	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
+	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
+}
+
+config.font = wezterm.font("MesloLGS Nerd Font Mono")
+config.font_size = 16
+config.line_height = 1
+
+config.enable_tab_bar = false
+
+config.window_decorations = "RESIZE"
+config.window_background_opacity = 1
+config.macos_window_background_blur = 0
+-- config.window_background_image = home .. "/.config/wezterm/assets/GL_even_8bit.png"
+config.window_background_image = constants.bg_img
+
+config.window_background_image_hsb = {
+	brightness = 0.1,
+}
+
+config.max_fps = 120
+
+config.prefer_egl = true
+
+-- adjust the columns and row width
+config.initial_cols = 90
+config.initial_rows = 30
+
+-- and finally, return the configuration to wezterm
+return config
