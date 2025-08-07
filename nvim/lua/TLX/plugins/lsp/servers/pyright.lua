eb20ffee8ef777750cd73607e0f926697e773f2a
tanlx-wow <uctanlx@gmail.com>
2025-08-07 15:29:25 -0400

    update

--- nvim/lua/TLX/plugins/lsp/servers/pyright.lua
+++ nvim/lua/TLX/plugins/lsp/servers/pyright.lua
@@ -5,17 +5,30 @@ return function()
 
 	lspconfig.pyright.setup({
 		root_dir = util.root_pattern("pixi.toml", "pyproject.toml", ".git"),
+
 		before_init = function(_, config)
+			print("[pyright] before_init called")
+
+			config.settings = config.settings or {}
+			config.settings.python = config.settings.python or {}
+
 			local root = util.root_pattern("pixi.toml", "pyproject.toml", ".git")(vim.fn.expand("%:p"))
 			local pixi_python = root .. "/.pixi/envs/default/bin/python"
-			if vim.fn.executable(pixi_python) == 1 then
-				config.settings = config.settings or {}
-				config.settings.python = config.settings.python or {}
-				config.settings.python.pythonPath = pixi_python
-			else
-				vim.notify("[pyright] Pixi Python not found: " .. pixi_python, vim.log.levels.WARN)
-			end
+			print("[pyright] injecting pythonPath = " .. pixi_python)
+
+			config.settings.python.pythonPath = pixi_python
 		end,
+		-- before_init = function(_, config)
+		-- 	local root = util.root_pattern("pixi.toml", "pyproject.toml", ".git")(vim.fn.expand("%:p"))
+		-- 	local pixi_python = root .. "/.pixi/envs/default/bin/python"
+		-- 	if vim.fn.executable(pixi_python) == 1 then
+		-- 		config.settings = config.settings or {}
+		-- 		config.settings.python = config.settings.python or {}
+		-- 		config.settings.python.pythonPath = pixi_python
+		-- 	else
+		-- 		vim.notify("[pyright] Pixi Python not found: " .. pixi_python, vim.log.levels.WARN)
+		-- 	end
+		-- end,
 		settings = {
 			python = {
 				analysis = {
