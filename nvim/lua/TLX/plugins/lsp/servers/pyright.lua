608c0ffac4eab93cf988aad4fae52b95c2707cb0
tanlx-wow <uctanlx@gmail.com>
2025-08-07 15:01:13 -0400

    update

---
+++ nvim/lua/TLX/plugins/lsp/servers/pyright.lua
@@ -0,0 +1,30 @@
+
+local lspconfig = require("lspconfig")
+local util = require("lspconfig.util")
+
+lspconfig.pyright.setup({
+  root_dir = util.root_pattern("pixi.toml", "pyproject.toml", ".git"),
+
+  before_init = function(_, config)
+    local root = config.root_dir or vim.fn.getcwd()
+    local pixi_python = root .. "/.pixi/envs/default/bin/python"
+
+    if vim.fn.executable(pixi_python) == 1 then
+      config.settings = config.settings or {}
+      config.settings.python = config.settings.python or {}
+      config.settings.python.pythonPath = pixi_python
+    else
+      vim.notify("[pyright] Pixi Python not found: " .. pixi_python, vim.log.levels.WARN)
+    end
+  end,
+
+  settings = {
+    python = {
+      analysis = {
+        autoSearchPaths = true,
+        diagnosticMode = "openFilesOnly",
+        useLibraryCodeForTypes = true,
+      },
+    },
+  },
+})
