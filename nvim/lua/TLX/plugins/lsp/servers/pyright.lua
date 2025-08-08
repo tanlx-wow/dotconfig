37db8fada74b6869b8f2b3481fcd22fb36c4cebe
tanlx-wow <uctanlx@gmail.com>
2025-08-07 21:11:22 -0400

    udpate

--- nvim/lua/TLX/plugins/lsp/servers/pyright.lua
+++ nvim/lua/TLX/plugins/lsp/servers/pyright.lua
@@ -2,15 +2,15 @@ return function(capabilities)
 	local cwd = vim.fn.getcwd()
 	local python_bin = cwd .. "/.pixi/envs/default/bin/python"
 
-	-- Dynamically get site-packages path
+	-- Get the absolute site-packages path
 	local function get_site_packages(pybin)
 		local handle = io.popen(pybin .. [[ -c "import site; print(site.getsitepackages()[0])" ]])
 		if not handle then
 			return nil
 		end
-		local result = handle:read("*a"):gsub("%s+", "")
+		local path = handle:read("*a"):gsub("%s+", "")
 		handle:close()
-		return result
+		return path
 	end
 
 	local site_packages = get_site_packages(python_bin)
@@ -20,16 +20,15 @@ return function(capabilities)
 		return
 	end
 
-	-- Optional: set PYRIGHT_PYTHON_PATH environment variable
-	vim.env.PYRIGHT_PYTHON_PATH = python_bin
-
 	require("lspconfig").pyright.setup({
 		capabilities = capabilities,
 		settings = {
 			python = {
 				pythonPath = python_bin,
+				venv = "default",
+				venvPath = ".pixi/envs",
 				analysis = {
-					autoSearchPaths = false,
+					autoSearchPaths = true,
 					diagnosticMode = "workspace",
 					typeCheckingMode = "basic",
 					useLibraryCodeForTypes = true,
