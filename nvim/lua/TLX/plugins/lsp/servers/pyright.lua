6fa77700713810613e4de94500327b8451206663
tanlx-wow <uctanlx@gmail.com>
2025-08-07 22:38:43 -0400

    update

--- nvim/lua/TLX/plugins/lsp/servers/pyright.lua
+++ nvim/lua/TLX/plugins/lsp/servers/pyright.lua
@@ -1,10 +1,22 @@
 return function(capabilities)
+	local lspconfig = require("lspconfig")
 	local cwd = vim.fn.getcwd()
 	local python_bin = cwd .. "/.pixi/envs/default/bin/python3"
 
+	-- Get Python site-packages path dynamically
+	local function get_site_packages(pybin)
+		local handle = io.popen(pybin .. [[ -c "import site; print(site.getsitepackages()[0])"]])
+		if not handle then
+			return nil
+		end
+		local result = handle:read("*a"):gsub("%s+", "")
+		handle:close()
+		return result
+	end
+
 	local function get_py_version(pybin)
 		local handle = io.popen(
-			pybin .. [[ -c "import sys; print('{}.{}'.format(sys.version_info.major, sys.version_info.minor))" ]]
+			pybin .. [[ -c "import sys; print('{}.{}'.format(sys.version_info.major, sys.version_info.minor))"]]
 		)
 		if not handle then
 			return nil
@@ -15,33 +27,29 @@ return function(capabilities)
 	end
 
 	local py_version = get_py_version(python_bin)
+	local site_packages = get_site_packages(python_bin)
 
-	if not py_version then
-		vim.notify("Failed to detect Python version from " .. python_bin, vim.log.levels.ERROR)
+	if not py_version or not site_packages then
+		vim.notify("[pyright] Could not detect Pixi Python version or site-packages", vim.log.levels.ERROR)
 		return
 	end
 
-	local lspconfig = require("lspconfig")
-
 	lspconfig.pyright.setup({
 		capabilities = capabilities,
+		root_dir = lspconfig.util.root_pattern("pyproject.toml", ".git", "requirements.txt"),
 		settings = {
 			python = {
-				pythonPath = python_bin,
-				venv = "default",
-				venvPath = ".pixi/envs",
 				analysis = {
 					autoSearchPaths = true,
 					diagnosticMode = "workspace",
 					typeCheckingMode = "basic",
 					useLibraryCodeForTypes = true,
-					extraPaths = {
-						cwd .. string.format("/.pixi/envs/default/lib/python%s/site-packages", py_version),
-					},
+					pythonPath = python_bin,
+					extraPaths = { site_packages },
 				},
 			},
 		},
 	})
 
-	vim.notify("Configured Pyright with Python " .. py_version, vim.log.levels.INFO)
+	vim.notify("✅ Configured Pyright with Python " .. py_version .. " from Pixi", vim.log.levels.INFO)
 end
