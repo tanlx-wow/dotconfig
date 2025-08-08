325fe1252169de81ec19d432795b65e25d385e30
tanlx-wow <uctanlx@gmail.com>
2025-08-07 20:14:40 -0400

    update

--- nvim/lua/TLX/plugins/lsp/servers/pyright.lua
+++ nvim/lua/TLX/plugins/lsp/servers/pyright.lua
@@ -2,6 +2,7 @@ return function(capabilities)
 	local cwd = vim.fn.getcwd()
 	local pixi_python = cwd .. "/.pixi/envs/default/bin/python"
 
+	-- Detect the python version (e.g., 3.13)
 	local function detect_python_version(python_bin)
 		local handle =
 			io.popen(python_bin .. " -c 'import sys; print(f\"{sys.version_info.major}.{sys.version_info.minor}\")'")
@@ -13,7 +14,7 @@ return function(capabilities)
 		return version
 	end
 
-	local python_version = detect_python_version(pixi_python)
+	local py_version = detect_python_version(pixi_python)
 
 	require("lspconfig").pyright.setup({
 		capabilities = capabilities,
@@ -28,7 +29,7 @@ return function(capabilities)
 					typeCheckingMode = "basic",
 					useLibraryCodeForTypes = true,
 					extraPaths = {
-						string.format(".pixi/envs/default/lib/python%s/site-packages", python_version),
+						string.format(".pixi/envs/default/lib/python%s/site-packages", py_version),
 					},
 				},
 			},
