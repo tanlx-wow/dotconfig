df8137e87fecc639374ee871850dd1e733418693
tanlx-wow <uctanlx@gmail.com>
2025-08-09 11:37:11 -0400

    update

---
+++ wezterm/tmux-func.lua
@@ -0,0 +1,25 @@
+local wezterm = require("wezterm")
+
+return {
+	leader = { key = "a", mods = "CTRL" }, -- like tmux's Ctrl+b
+
+	keys = {
+		-- Split panes
+		{ key = "-", mods = "LEADER", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+		{ key = "|", mods = "LEADER", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+
+		-- Move between panes
+		{ key = "h", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Left") },
+		{ key = "l", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Right") },
+		{ key = "k", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Up") },
+		{ key = "j", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Down") },
+
+		-- Tabs
+		{ key = "c", mods = "LEADER", action = wezterm.action.SpawnTab("CurrentPaneDomain") },
+		{ key = "n", mods = "LEADER", action = wezterm.action.ActivateTabRelative(1) },
+		{ key = "p", mods = "LEADER", action = wezterm.action.ActivateTabRelative(-1) },
+
+		-- Workspaces (like tmux sessions)
+		{ key = "w", mods = "LEADER", action = wezterm.action.ShowLauncherArgs({ flags = "WORKSPACES" }) },
+	},
+}
