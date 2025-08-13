66b795280a772e62d6dcf314194a07aa7a64bed3
tanlx-wow <uctanlx@gmail.com>
2025-08-13 13:42:40 -0400

    update

--- nvim/lua/TLX/plugins/nvim-biscuits.lua
+++ nvim/lua/TLX/plugins/nvim-biscuits.lua
@@ -4,8 +4,16 @@ return {
 	config = function()
 		require("nvim-biscuits").setup({
 			toggle_keybind = "<leader>cb",
-			show_on_start = true, -- defaults to false
-			prefix_string = " 📎 ",
+			show_on_start = false, -- don't show immediately
+			prefix_string = " 🫷📎",
+		})
+
+		-- Only show biscuits when the cursor stays on a line for a moment
+		vim.api.nvim_create_autocmd("CursorHold", {
+			callback = function()
+				-- force refresh biscuits for the current line
+				require("nvim-biscuits").refresh_biscuits()
+			end,
 		})
 	end,
 }
