ff702f6e5f9675173fb5ee9a73ab5cd179f6395f
tanlx-wow <uctanlx@gmail.com>
2025-08-13 13:52:09 -0400

    update

--- nvim/lua/TLX/plugins/nvim-biscuits.lua
+++ nvim/lua/TLX/plugins/nvim-biscuits.lua
@@ -5,7 +5,7 @@ return {
 		require("nvim-biscuits").setup({
 			show_on_start = false, -- don't draw on buffer load
 			cursor_line_only = true, -- only show on the line with the cursor
-			prefix_string = " 📎 ",
+			prefix_string = " 🫷📎 ",
 			-- optional tuning:
 			-- default_config = { max_length = 80, min_distance = 5 },
 		})
