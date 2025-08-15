8bb0144c126b6b0a903818f6dfbd0b768dd5d89b
tanlx-wow <uctanlx@gmail.com>
2025-08-15 14:55:19 -0400

    update

--- nvim/lua/TLX/plugins/colorscheme.lua
+++ nvim/lua/TLX/plugins/colorscheme.lua
@@ -41,6 +41,44 @@ return {
 			})
 			-- load the colorscheme here
 			vim.cmd([[colorscheme tokyonight]])
+			-- 1) Truecolor on
+			vim.opt.termguicolors = true
+
+			-- 2) Make floats look like normal windows (helps LazyGit)
+			vim.api.nvim_set_hl(0, "NormalFloat", { link = "Normal" })
+			vim.api.nvim_set_hl(0, "FloatBorder", { link = "Normal" })
+
+			-- 3) Copy your WezTerm ANSI/brights into Neovim's terminal palette
+			-- WezTerm colors you shared:
+			local ansi = {
+				"#214969",
+				"#E52E2E",
+				"#44FFB1",
+				"#FFE073",
+				"#0FC5ED",
+				"#a277ff",
+				"#24EAF7",
+				"#24EAF7",
+			}
+			local brights = {
+				"#214969",
+				"#E52E2E",
+				"#44FFB1",
+				"#FFE073",
+				"#A277FF",
+				"#a277ff",
+				"#24EAF7",
+				"#24EAF7",
+			}
+
+			for i = 0, 7 do
+				vim.g["terminal_color_" .. i] = ansi[i + 1]
+				vim.g["terminal_color_" .. (i + 8)] = brights[i + 1]
+			end
+
+			-- 4) Optional: match terminal cursor colors (used by :terminal / LazyGit)
+			vim.api.nvim_set_hl(0, "TermCursor", { fg = "#011423", bg = "#47FF9C" })
+			vim.api.nvim_set_hl(0, "TermCursorNC", { fg = "#011423", bg = "#47FF9C" })
 		end,
 	},
 }
