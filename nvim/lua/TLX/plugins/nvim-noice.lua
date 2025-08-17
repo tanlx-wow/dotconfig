52298969b98e61fef6639bc96730e220f5b8b5d9
tanlx-wow <uctanlx@gmail.com>
2025-08-17 16:10:39 -0400

    update

---
+++ nvim/lua/TLX/plugins/nvim-noice.lua
@@ -0,0 +1,28 @@
+return {
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+		"rcarriga/nvim-notify",
+	},
+	config = function()
+		require("noice").setup({
+			cmdline = {
+				enabled = true, -- enable the command-line UI
+				view = "cmdline_popup", -- show in the middle
+			},
+			views = {
+				cmdline_popup = {
+					position = {
+						row = "50%",
+						col = "50%",
+					},
+					size = {
+						width = 60,
+						height = "auto",
+					},
+				},
+			},
+		})
+	end,
+}
