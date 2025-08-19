c6e781e0d2ec408dfaf8374a8473f232c3064239
tanlx-wow <uctanlx@gmail.com>
2025-08-19 15:53:12 -0400

    update

--- nvim/lua/TLX/plugins/colorscheme.lua
+++ nvim/lua/TLX/plugins/colorscheme.lua
@@ -4,16 +4,16 @@ return {
 		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
 			local transparent = true
-			-- local bg = "#011628"
-			-- local bg_dark = "#011423"
-			-- local bg_highlight = "#143652"
-			-- local bg_search = "#0A64AC"
-			-- local bg_visual = "#275378"
-			-- local fg = "#CBE0F0"
-			-- local fg_dark = "#B4D0E9"
-			-- local fg_gutter = "#627E97"
-			-- local border = "#547998"
-			--
+			local bg = "#011628"
+			local bg_dark = "#011423"
+			local bg_highlight = "#143652"
+			local bg_search = "#0A64AC"
+			local bg_visual = "#275378"
+			local fg = "#CBE0F0"
+			local fg_dark = "#B4D0E9"
+			local fg_gutter = "#627E97"
+			local border = "#547998"
+
 			require("tokyonight").setup({
 				style = "night",
 				transparent = transparent,
