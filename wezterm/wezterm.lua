75ae1d7103e9f7ad1f48ed81131ed535ef53e661
tanlx-wow <uctanlx@gmail.com>
2025-08-19 15:59:35 -0400

    update

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -14,7 +14,7 @@ local config = wezterm.config_builder()
 
 local home = wezterm.home_dir
 
-config.color_scheme = "Tokyo Night(Gogh)"
+config.color_scheme = "tokyonight"
 -- coolnight colorscheme
 
 -- config.colors = {
