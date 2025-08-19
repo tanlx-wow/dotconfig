f1719ae589169d9e8a687141d9195a5018951900
tanlx-wow <uctanlx@gmail.com>
2025-08-19 16:10:33 -0400

    update

--- nvim/lua/TLX/plugins/colorscheme.lua
+++ nvim/lua/TLX/plugins/colorscheme.lua
@@ -8,7 +8,7 @@ return {
 			-- Configure Tokyonight with the "moon" style
 			require("tokyonight").setup({
 				style = "moon", -- <-- pick the "moon" variant
-				transparent = false, -- set to true if you want transparent bg
+				transparent = true, -- set to true if you want transparent bg
 			})
 
 			-- Load the colorscheme
@@ -19,35 +19,9 @@ return {
 			vim.api.nvim_set_hl(0, "NormalFloat", { link = "Normal" })
 			vim.api.nvim_set_hl(0, "FloatBorder", { link = "Normal" })
 
-			-- Optional: set your own terminal palette
-			local ansi = {
-				"#214969",
-				"#E52E2E",
-				"#44FFB1",
-				"#FFE073",
-				"#0FC5ED",
-				"#a277ff",
-				"#24EAF7",
-				"#24EAF7",
-			}
-			local brights = {
-				"#214969",
-				"#E52E2E",
-				"#44FFB1",
-				"#FFE073",
-				"#A277FF",
-				"#a277ff",
-				"#24EAF7",
-				"#24EAF7",
-			}
-			for i = 0, 7 do
-				vim.g["terminal_color_" .. i] = ansi[i + 1]
-				vim.g["terminal_color_" .. (i + 8)] = brights[i + 1]
-			end
-
 			-- Terminal cursor highlight
-			vim.api.nvim_set_hl(0, "TermCursor", { fg = "#011423", bg = "#47FF9C" })
-			vim.api.nvim_set_hl(0, "TermCursorNC", { fg = "#011423", bg = "#47FF9C" })
+			-- vim.api.nvim_set_hl(0, "TermCursor", { fg = "#011423", bg = "#47FF9C" })
+			-- vim.api.nvim_set_hl(0, "TermCursorNC", { fg = "#011423", bg = "#47FF9C" })
 		end,
 	},
 }
