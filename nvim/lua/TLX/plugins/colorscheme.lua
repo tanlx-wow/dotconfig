6dc99238532633d7bfc8c88bdd517b658e13f09f
tanlx-wow <uctanlx@gmail.com>
2025-08-19 16:03:33 -0400

    update

--- nvim/lua/TLX/plugins/colorscheme.lua
+++ nvim/lua/TLX/plugins/colorscheme.lua
@@ -1,18 +1,17 @@
-{
-  "folke/tokyonight.nvim",
-  lazy = false,   -- load immediately
-  priority = 1000, -- make sure it loads before other UI stuff
+return {
+	"folke/tokyonight.nvim",
+	lazy = false, -- load immediately
+	priority = 1000, -- make sure it loads before other UI stuff
 
-  config = function()
-    -- 1) Set the colorscheme
-    vim.cmd([[colorscheme tokyonight]])
+	config = function()
+		-- 1) Set the colorscheme
+		vim.cmd([[colorscheme tokyonight_night]])
 
-    -- 2) Enable true color
-    vim.opt.termguicolors = true
+		-- 2) Enable true color
+		vim.opt.termguicolors = true
 
-    -- 3) Make floats look like normal windows (helps LazyGit, Telescope, etc.)
-    vim.api.nvim_set_hl(0, "NormalFloat", { link = "Normal" })
-    vim.api.nvim_set_hl(0, "FloatBorder", { link = "Normal" })
-  end,
+		-- 3) Make floats look like normal windows (helps LazyGit, Telescope, etc.)
+		vim.api.nvim_set_hl(0, "NormalFloat", { link = "Normal" })
+		vim.api.nvim_set_hl(0, "FloatBorder", { link = "Normal" })
+	end,
 }
-
