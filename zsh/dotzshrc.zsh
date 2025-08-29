27001fae6b7a127b14f78a049a1a532cd9b7564c
tanlx-wow <uctanlx@gmail.com>
2025-08-29 14:39:01 -0400

    update

--- zsh/dotzshrc.zsh
+++ zsh/dotzshrc.zsh
@@ -1,5 +1,7 @@
 source ~/.bash_profile
 
+fastfetch
+
 # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
 # Initialization code that may require console input (password prompts, [y/n]
 # confirmations, etc.) must go above this block; everything else may go below.
