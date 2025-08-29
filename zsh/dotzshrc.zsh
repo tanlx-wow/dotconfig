3a67cc4ea160b6fc2590c7e894fe7a5049ba8ba1
tanlx-wow <uctanlx@gmail.com>
2025-08-29 14:41:05 -0400

    update

--- zsh/dotzshrc.zsh
+++ zsh/dotzshrc.zsh
@@ -1,6 +1,6 @@
 source ~/.bash_profile
 
-fastfetch
+fastfetch --logo nixos
 
 # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
 # Initialization code that may require console input (password prompts, [y/n]
