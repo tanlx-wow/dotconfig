9a486b0fd2b79311f68602060e8abf43b6b3b149
tanlx-wow <uctanlx@gmail.com>
2025-09-06 16:32:55 -0400

    update

--- shell_tool/app_id.sh
+++ shell_tool/app_id.sh
@@ -1,5 +1,11 @@
-echo "please input the name of application you want darkmode off"
-read app_name
+
+# if no argument provided, prompt user
+if [ "$1" = "" ]; then
+  echo "please input the name of application you want darkmode off"
+  read app_name
+else 
+  app_name="$1"
+fi
 
 app_id=$(osascript -e "id of app \"$app_name\"")
 echo "$app_id"
