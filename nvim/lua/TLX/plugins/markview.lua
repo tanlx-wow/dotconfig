7eea0b53dc2d656eb58acce2d5f8b561381af9a6
tanlx-wow <uctanlx@gmail.com>
2025-10-22 16:46:06 -0400

    update

---
+++ nvim/lua/TLX/plugins/markview.lua
@@ -0,0 +1,11 @@
+-- For `plugins/markview.lua` users.
+return {
+	"OXY2DEV/markview.nvim",
+	lazy = true,
+
+	-- For blink.cmp's completion
+	-- source
+	-- dependencies = {
+	--     "saghen/blink.cmp"
+	-- },
+}
