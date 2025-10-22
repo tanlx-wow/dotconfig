05d0dd6f557374cd0916e22eb6b49d3de674ee9c
tanlx-wow <uctanlx@gmail.com>
2025-10-22 16:19:37 -0400

    update

--- nvim/lua/TLX/plugins/render-markdown.lua
+++ nvim/lua/TLX/plugins/render-markdown.lua
@@ -62,6 +62,26 @@ return {
 					above = 0,
 					below = 0,
 				},
+				latex = {
+					-- Turn on / off latex rendering.
+					enabled = true,
+					-- Additional modes to render latex.
+					render_modes = false,
+					-- Executable used to convert latex formula to rendered unicode.
+					-- If a list is provided the first command available on the system is used.
+					converter = { "utftex", "latex2text" },
+					-- Highlight for latex blocks.
+					highlight = "RenderMarkdownMath",
+					-- Determines where latex formula is rendered relative to block.
+					-- | above  | above latex block                               |
+					-- | below  | below latex block                               |
+					-- | center | centered with latex block (must be single line) |
+					position = "center",
+					-- Number of empty lines above latex blocks.
+					top_pad = 0,
+					-- Number of empty lines below latex blocks.
+					bottom_pad = 0,
+				},
 			})
 		end,
 	},
