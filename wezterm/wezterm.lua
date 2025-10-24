2ee344f934fb88e4464d7a5229b3a6c50ed87d33
tanlx-wow <uctanlx@gmail.com>
2025-10-24 18:03:02 -0400

    update

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -36,8 +36,8 @@ config.line_height = 1
 config.enable_tab_bar = false
 
 config.window_decorations = "RESIZE"
-config.window_background_opacity = 0.8
-config.macos_window_background_blur = 100
+config.window_background_opacity = 0.9
+config.macos_window_background_blur = 10
 -- config.window_background_image = home .. "/.config/wezterm/assets/GL_even_8bit.png"
 -- config.window_background_image = constants.bg_img
 
