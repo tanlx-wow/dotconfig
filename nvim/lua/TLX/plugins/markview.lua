8e443e744daa0b02a37e1937cc11159a7d86788d
tanlx-wow <uctanlx@gmail.com>
2025-10-24 17:02:44 -0400

    update

--- nvim/lua/TLX/plugins/markview.lua
+++ nvim/lua/TLX/plugins/markview.lua
@@ -2,6 +2,15 @@
 return {
 	"OXY2DEV/markview.nvim",
 	lazy = false,
+	config = function()
+		local presets = require("markview.presets")
+
+		require("markview").setup({
+			markdown = {
+				headings = presets.headings.slanted,
+			},
+		})
+	end,
 
 	-- For blink.cmp's completion
 	-- source
