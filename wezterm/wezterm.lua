7bea8c44605da613e9cd7f8128adc37724bf9b0b
tanlx-wow <uctanlx@gmail.com>
2025-10-24 17:42:33 -0400

    udpate

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -14,20 +14,20 @@ local config = wezterm.config_builder()
 
 local home = wezterm.home_dir
 
--- config.color_scheme = "Tokyo Night Moon"
+config.color_scheme = "Tokyo Night Moon"
 -- coolnight colorscheme
 
-config.colors = {
-	foreground = "#CBE0F0",
-	background = "#011423",
-	cursor_bg = "#47FF9C",
-	cursor_border = "#47FF9C",
-	cursor_fg = "#011423",
-	selection_bg = "#033259",
-	selection_fg = "#CBE0F0",
-	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
-	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
-}
+-- config.colors = {
+-- 	foreground = "#CBE0F0",
+-- 	background = "#011423",
+-- 	cursor_bg = "#47FF9C",
+-- 	cursor_border = "#47FF9C",
+-- 	cursor_fg = "#011423",
+-- 	selection_bg = "#033259",
+-- 	selection_fg = "#CBE0F0",
+-- 	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
+-- 	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
+-- }
 
 config.font = wezterm.font("MesloLGS Nerd Font Mono")
 config.font_size = 16
