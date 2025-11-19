e9db82203eaa85e9bc72590a4a298e2b69c1bf83
tanlx-wow <uctanlx@gmail.com>
2025-11-19 13:56:21 -0500

    update

--- nvim/lua/TLX/plugins/minimap.lua
+++ nvim/lua/TLX/plugins/minimap.lua
@@ -4,8 +4,8 @@ return {
 	lazy = false, -- load on startup
 	config = function()
 		-- Basic recommended settings
-		vim.g.minimap_width = 0.1
+		vim.g.minimap_width = 1
 		vim.g.minimap_auto_start = 1
-		vim.g.minimap_auto_start_win_enter = 0.1
+		vim.g.minimap_auto_start_win_enter = 1
 	end,
 }
