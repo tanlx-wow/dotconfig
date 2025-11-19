e5c71a9ab0edbfc356ceb039262af44529b6f826
tanlx-wow <uctanlx@gmail.com>
2025-11-19 14:56:25 -0500

    udpate

--- nvim/lua/TLX/plugins/minimap.lua
+++ nvim/lua/TLX/plugins/minimap.lua
@@ -4,9 +4,14 @@ return {
 	lazy = false, -- load on startup
 	init = function()
 		-- Basic recommended settings
-		vim.g.minimap_width = 2
+		vim.g.minimap_width = 10
 		vim.g.minimap_auto_start = 0
 		vim.g.minimap_auto_start_win_enter = 0
 		vim.g.minimap_side = "right"
+		vim.g.minimap_base_highlight = "Normal"
+		vim.g.minimap_block_filetypes = { "fugitive", "nerdtree", "tagbar" }
+		vim.g.minimap_close_filetypes = { "startify", "netrw", "vim-plug" }
+		vim.g.minimap_git_colors = 1
+		vim.g.minimap_highlight_range = 1
 	end,
 }
