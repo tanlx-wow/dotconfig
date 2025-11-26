80659e15f93feae687e80feb9d8e1fa6b07a1f45
tanlx-wow <uctanlx@gmail.com>
2025-11-26 11:30:05 -0500

    update

--- nvim/lua/TLX/plugins/opencode.lua
+++ nvim/lua/TLX/plugins/opencode.lua
@@ -13,48 +13,23 @@ return {
 		vim.opt.autoread = true
 
 		-- Recommended/example keymaps
-		vim.keymap.set("n", "<leader>ot", function()
-			require("opencode").toggle()
-		end, { desc = "Toggle embedded" })
-
-		vim.keymap.set("n", "<leader>oA", function()
-			require("opencode").ask()
-		end, { desc = "Ask" })
-
-		vim.keymap.set("n", "<leader>oa", function()
-			require("opencode").ask("@cursor: ")
-		end, { desc = "Ask about this" })
-
-		vim.keymap.set("v", "<leader>oa", function()
-			require("opencode").ask("@selection: ")
-		end, { desc = "Ask about selection" })
-
-		vim.keymap.set("n", "<leader>oe", function()
-			require("opencode").prompt("Explain @cursor and its context")
-		end, { desc = "Explain this code" })
-
-		vim.keymap.set("n", "<leader>o+", function()
-			require("opencode").prompt("@buffer", { append = true })
-		end, { desc = "Add buffer to prompt" })
-
-		vim.keymap.set("v", "<leader>o+", function()
-			require("opencode").prompt("@selection", { append = true })
-		end, { desc = "Add selection to prompt" })
-
-		vim.keymap.set("n", "<leader>on", function()
-			require("opencode").command("session_new")
-		end, { desc = "New session" })
-
-		vim.keymap.set("n", "<leader>ou", function()
-			require("opencode").command("messages_half_page_up")
-		end, { desc = "Messages half page up" })
-
-		vim.keymap.set("n", "<leader>od", function()
-			require("opencode").command("messages_half_page_down")
-		end, { desc = "Messages half page down" })
-
-		vim.keymap.set({ "n", "v" }, "<leader>os", function()
+		vim.keymap.set({ "n", "x" }, "<leader>oa", function()
+			require("opencode").ask("@this: ", { submit = true })
+		end, { desc = "Ask opencode" })
+		vim.keymap.set({ "n", "x" }, "<leader>ox", function()
 			require("opencode").select()
-		end, { desc = "Select prompt" })
+		end, { desc = "Execute opencode action…" })
+		vim.keymap.set({ "n", "x" }, "<leader>og", function()
+			require("opencode").prompt("@this")
+		end, { desc = "Add to opencode" })
+		vim.keymap.set({ "n", "t" }, "<leader>ot", function()
+			require("opencode").toggle()
+		end, { desc = "Toggle opencode" })
+		vim.keymap.set("n", "<leader>ou", function()
+			require("opencode").command("session.half.page.up")
+		end, { desc = "opencode half page up" })
+		vim.keymap.set("n", "<leader>od", function()
+			require("opencode").command("session.half.page.down")
+		end, { desc = "opencode half page down" })
 	end,
 }
