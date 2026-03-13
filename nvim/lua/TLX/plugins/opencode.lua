753f27463eb1c11b6c4511cb4acb53372c8d3600
tanlx-wow <uctanlx@gmail.com>
2026-03-13 11:18:27 -0400

    opencode and lspconfig adjust
    opencode.lua clean up and lspconfig symbol add back

--- nvim/lua/TLX/plugins/opencode.lua
+++ nvim/lua/TLX/plugins/opencode.lua
@@ -1,39 +1,3 @@
--- return {
--- 	"NickvanDyke/opencode.nvim",
--- 	dependencies = {
--- 		-- Recommended for better prompt input, and required to use `opencode.nvim`'s embedded terminal — otherwise optional
--- 		{ "folke/snacks.nvim", opts = { input = {}, picker = {}, terminal = {} } },
--- 	},
--- 	config = function()
--- 		vim.g.opencode_opts = {
--- 			-- Your configuration, if any — see `lua/opencode/config.lua`
--- 		}
---
--- 		-- Required for `opts.auto_reload`
--- 		vim.opt.autoread = true
---
--- 		-- Recommended/example keymaps
--- 		vim.keymap.set({ "n", "x" }, "<leader>oa", function()
--- 			require("opencode").ask("@this: ", { submit = true })
--- 		end, { desc = "Ask opencode" })
--- 		vim.keymap.set({ "n", "x" }, "<leader>ox", function()
--- 			require("opencode").select()
--- 		end, { desc = "Execute opencode action…" })
--- 		vim.keymap.set({ "n", "x" }, "<leader>og", function()
--- 			require("opencode").prompt("@this")
--- 		end, { desc = "Add to opencode" })
--- 		vim.keymap.set({ "n", "t" }, "<leader>ot", function()
--- 			require("opencode").toggle()
--- 		end, { desc = "Toggle opencode" })
--- 		vim.keymap.set("n", "<leader>ou", function()
--- 			require("opencode").command("session.half.page.up")
--- 		end, { desc = "opencode half page up" })
--- 		vim.keymap.set("n", "<leader>od", function()
--- 			require("opencode").command("session.half.page.down")
--- 		end, { desc = "opencode half page down" })
--- 	end,
--- }
-
 return {
 	"nickjvandyke/opencode.nvim",
 	version = "*", -- Latest stable release
