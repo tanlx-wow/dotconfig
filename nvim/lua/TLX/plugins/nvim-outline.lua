05f5366c6e6c92ca1c188b2900cb3b2a0e42e293
tanlx-wow <uctanlx@gmail.com>
2026-06-10 11:38:48 -0400

    nvim-outline udpate for auto-width

--- nvim/lua/TLX/plugins/nvim-outline.lua
+++ nvim/lua/TLX/plugins/nvim-outline.lua
@@ -1,14 +1,39 @@
 return {
 	"hedyhli/outline.nvim",
 	config = function()
+		local outline_width_percentage = 20
+
 		--  toggle outline
 		vim.keymap.set("n", "<leader>o", "<cmd>Outline<CR>", { desc = "Toggle Outline" })
 
 		require("outline").setup({
 			outline_window = {
-				width = 20,
+				width = outline_width_percentage,
 				relative_width = true,
 			},
+		})
+
+		vim.api.nvim_create_autocmd({ "VimResized", "WinResized" }, {
+			group = vim.api.nvim_create_augroup("OutlineResize", { clear = true }),
+			desc = "Resize outline window when nvim window got resized",
+			callback = function()
+				local ok, outline = pcall(require, "outline")
+				if not ok or not outline.is_open() then
+					return
+				end
+
+				local sidebar = outline._get_sidebar(false)
+				local win = sidebar and sidebar.view and sidebar.view.win
+
+				if not win or not vim.api.nvim_win_is_valid(win) then
+					return
+				end
+
+				local width = math.ceil(vim.o.columns * (outline_width_percentage / 100))
+				if vim.api.nvim_win_get_width(win) ~= width then
+					vim.api.nvim_win_set_width(win, width)
+				end
+			end,
 		})
 
 		-- NEW: Automatically open the outline for Markdown files
